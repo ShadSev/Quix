00e41eeb1079e336bee5bbbd64054a5160f1ffdb
Mathias F <junk@fernstedt.se>
2016-09-28 16:32:43 +0200

    Update #2

--- src/main/java/se/quiz/QuizController.java
+++ src/main/java/se/quiz/QuizController.java
@@ -2,13 +2,9 @@ package se.quiz;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
